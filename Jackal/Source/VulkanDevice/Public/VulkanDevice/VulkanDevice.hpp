004b23f581577293954f74dc90d302d8aaea69fe
Mario Garcia <alexander.garcia18@yahoo.com>
2017-06-02 23:05:57 -0700

    Setting up rendering modules

---
+++ Jackal/Source/VulkanDevice/Public/VulkanDevice/VulkanDevice.hpp
@@ -0,0 +1,20 @@
+// Copyright (c) 2017 Jackal Engine, MIT License.
+#pragma once
+
+
+#include "RenderDevice/RenderDevice.hpp"
+
+
+namespace jkl {
+
+
+// Vulkan Render Device, which handles all of Vulkan rendering API and 
+// whatnot. Use this device to handle drawing on the screen.
+class VulkanDevice : public RenderDevice {
+public:
+
+
+private:
+
+};
+} // jkl
