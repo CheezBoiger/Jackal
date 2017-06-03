d06a95772306c5d302887d3547c17f7960be92e8
Mario Garcia <alexander.garcia18@yahoo.com>
2017-06-02 23:44:28 -0700

    Added OpenGL Device set up

---
+++ Jackal/Source/OpenGLDevice/Public/OpenGLDevice/OpenGLDevice.hpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2017 Jackal Engine, MIT License.
+#pragma once
+
+
+#include "RenderDevice/RenderDevice.hpp"
+
+
+namespace jkl {
+
+
+// OpenGL Rendering Device.
+class OpenGLDevice : public RenderDevice {
+public:
+
+private:
+};
+} // jkl
