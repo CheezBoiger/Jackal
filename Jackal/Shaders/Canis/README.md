0e1b4cc17518a0625f00d938a6f4d9373cb0bf16
Mario Garcia <alexander.garcia18@yahoo.com>
2017-06-16 22:56:49 -0700

    Adding Shader abstract

---
+++ Jackal/Shaders/Canis/README.md
@@ -0,0 +1,4 @@
+# Canis Shader Language
+Canis is an abstract, High Level Shader language. It is intended to work with
+our Canis Shader Compiler, to which will then convert our homebrewed language into
+GLSL or HLSL.
--- Jackal/Source/RenderDevice/Public/RenderDevice/Shader.hpp
+++ Jackal/Source/RenderDevice/Public/RenderDevice/Shader.hpp
@@ -7,24 +7,41 @@
 namespace jkl {
 
 
-// Shader configuration. Abstract Shader.
+// Shader configuration. Abstract Shader. Used overall throughout the
+// implmentation of the Rendering Device.
 class Shader {
 public:
   enum Type {
+    None,
     Vertex,
     Fragment,
     Geometry,
     Compute,
     TessEvaluation,
     TessControl,
+    Pixel = Fragment,
+    Hull = TessControl,
+    Domain = TessEvaluation
   };
 
-  virtual ~Shader();  
+  virtual ~Shader() { }
 
-  Shader(Type shaderType);
+  Shader(const JString name = JTEXT("Default"),
+        const JString filepath = JTEXT(""));
 
+  // Compiles the shader. This is dependent on the rendering api. 
+  virtual bool8 Compile(const JString filepath) = 0;
+
+  JString &SourceCode() const;
+
+  Type ShaderType() const { return shaderType; }
+  JString Name() const { return shaderName; }
+  bool8 Compiled() const { return compiled; }
+
+  
 protected:
   JString shaderName;
   Type    shaderType;
+  bool8   compiled;
 };
 } // jkl
